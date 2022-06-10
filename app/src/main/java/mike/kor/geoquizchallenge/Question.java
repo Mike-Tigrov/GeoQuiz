1e8ff0689bb35f557b3778b5215fefda891165af
Mike-Tigrov <xiwnik@yahoo.com>
2022-06-10 20:07:54 +0300

    Some new code

--- app/src/main/java/mike/kor/geoquizchallenge/Question.java
+++ app/src/main/java/mike/kor/geoquizchallenge/Question.java
@@ -1,4 +1,14 @@
 package mike.kor.geoquizchallenge;
 
 public class Question {
+
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Question(int textResId, boolean answerTrue) {
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+
+
 }
